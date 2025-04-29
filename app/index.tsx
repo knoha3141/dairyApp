e1a3113fd6ce066eca9c5a321e6d7cc62b07a8b3
koheiNohara <k.nohara@flux-g.com>
2025-04-29 22:53:35 +0900

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>今日の日記</Text>
+    </View>
+  );
+}
